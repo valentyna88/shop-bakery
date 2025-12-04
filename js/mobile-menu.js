f029b3beb996b920cee99eb4516cd2d8e0b1e649
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-12-04 18:34:20 +0200

    feat(mobile-menu): close on link click and disable body scroll

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,8 +1,19 @@
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuBtnOpen = document.querySelector('.menu-btn-open');
 const menuBtnClose = document.querySelector('.menu-btn-close');
+const menuLinks = document.querySelectorAll('.mobile-menu__link');
 
-const toggleMenu = () => mobileMenu.classList.toggle('is-open');
+const toggleMenu = () => {
+  mobileMenu.classList.toggle('is-open');
+  document.body.classList.toggle('no-scroll');
+};
 
 menuBtnOpen.addEventListener('click', toggleMenu);
 menuBtnClose.addEventListener('click', toggleMenu);
+
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    mobileMenu.classList.remove('is-open');
+    document.body.classList.remove('no-scroll');
+  });
+});
