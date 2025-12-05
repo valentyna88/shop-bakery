847df540d6a0fbb7a48839c2cda35502dadb2b57
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-12-05 19:09:58 +0200

    fix: enable modal open on all “callback” buttons

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,11 @@
 const modal = document.querySelector('.backdrop');
-const modalBtnOpen = document.querySelector('.modal-btn-open');
 const modalBtnClose = document.querySelector('.modal-btn-close');
+const modalOpenButtons = document.querySelectorAll('.modal-btn-open');
 
 const toggleModal = () => modal.classList.toggle('is-hidden');
 
-modalBtnOpen.addEventListener('click', toggleModal);
+modalOpenButtons.forEach(button => {
+  button.addEventListener('click', toggleModal);
+});
+
 modalBtnClose.addEventListener('click', toggleModal);
